a0cc71b1fe1a73dfcc5e36e2aab63b810e8f87c6
andq <dangquocan95@gmail.com>
2021-06-02 08:59:27 +0700

    remove attention gate

--- unetFullAttention/unet_parts.py
+++ unetFullAttention/unet_parts.py
@@ -73,7 +73,7 @@ class Up(nn.Module):
     def forward(self, x1, x2):
         if self.enable_attention:
             x1 = self.attention(x1)
-            x2 = self.attention_gate(x2)
+            # x2 = self.attention_gate(x2)
 
         x1 = self.up(x1)
 
