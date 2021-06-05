7043563137548a913e65d3db010f41332d162ed7
andq <dangquocan95@gmail.com>
2021-06-05 16:32:06 +0700

    fix gate dimension

--- unetGate/unet_parts.py
+++ unetGate/unet_parts.py
@@ -56,10 +56,11 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels)
 
         self.attention = Attention_block(
-            F_g=out_channels, F_l=out_channels, F_int=out_channels//2)
+            F_g=in_channels // 2, F_l=in_channels // 2, F_int=in_channels//4)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
+        x1 = self.attention(g=x1, x=x2)
 
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
@@ -71,7 +72,7 @@ class Up(nn.Module):
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
 
-        x1 = self.attention(g=x1, x=x2)
+
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
