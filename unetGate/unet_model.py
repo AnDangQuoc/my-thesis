b1010c664cf7e06c67faa83ebffa3161b8961adb
andq <dangquocan95@gmail.com>
2021-06-05 16:38:32 +0700

    fix model to use retrain

--- unetGate/unet_model.py
+++ unetGate/unet_model.py
@@ -4,11 +4,8 @@ import torch.nn.functional as F
 import torch
 from .unet_parts import *
 
-from .coordattention import *
-
-
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True, has_attention=True):
+    def __init__(self, n_channels, n_classes, bilinear=True, enable_attention=True):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
@@ -19,34 +16,22 @@ class UNet(nn.Module):
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
-
         self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.attention1 = Attention_block(
-            F_g=512 // factor, F_l=512 // factor, F_int=256 // factor)
-
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.attention2 = Attention_block(
-            F_g=256 // factor, F_l=256 // factor, F_int=128 // factor)
-
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.attention3 = Attention_block(
-            F_g=128 // factor, F_l=128 // factor, F_int=64 // factor)
-
-        self.up4 = Up(128, 64, bilinear)
-        self.attention4 = Attention_block(
-            F_g=64 // factor, F_l=64 // factor, F_int=32 // factor)
-        
+        self.up1 = Up(1024, 512 // factor, bilinear,
+                      enable_attention=enable_attention)
+        self.up2 = Up(512, 256 // factor, bilinear,
+                      enable_attention=enable_attention)
+        self.up3 = Up(256, 128 // factor, bilinear,
+                      enable_attention=enable_attention)
+        self.up4 = Up(128, 64, bilinear, enable_attention=enable_attention)
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
-
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
--- unetGate/unet_parts.py
+++ unetGate/unet_parts.py
@@ -42,9 +42,9 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, bilinear=True, enable_attention=True):
         super().__init__()
-
+        self.enable_attention = enable_attention
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(
@@ -53,14 +53,15 @@ class Up(nn.Module):
         else:
             self.up = nn.ConvTranspose2d(
                 in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.attention = Attention_block(
+                F_g=in_channels // 2, F_l=in_channels // 2, F_int=in_channels//4)
             self.conv = DoubleConv(in_channels, out_channels)
-
-        self.attention = Attention_block(
-            F_g=in_channels // 2, F_l=in_channels // 2, F_int=in_channels//4)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        x1 = self.attention(g=x1, x=x2)
+
+        if self.enable_attention:
+            x1 = self.attention(g=x1, x=x2)
 
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
@@ -71,7 +72,6 @@ class Up(nn.Module):
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-
 
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
