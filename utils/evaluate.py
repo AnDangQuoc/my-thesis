8313977d93b8a870f84419762ba765b5a97f8403
andq <dangquocan95@gmail.com>
2021-06-25 10:03:19 +0700

    remove evaluate

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,12 +1,12 @@
-import numpy as np
-import seg_metrics.seg_metrics as sg
+# import numpy as np
+# import seg_metrics.seg_metrics as sg
 
-LABELS = [1, 2, 3]
+# LABELS = [1, 2, 3, 4]
 
 
-def eval(groundTruth, predict, outFile='result.csv'):
+# def eval(groundTruth, predict, outFile='result.csv'):
 
-    metrics = sg.write_metrics(labels=LABELS,
-                               gdth_path=groundTruth,
-                               pred_path=predict, csv_file=outFile)
-    return metrics
+#     metrics = sg.write_metrics(labels=LABELS,
+#                                gdth_path=groundTruth,
+#                                pred_path=predict, csv_file=outFile)
+#     return metrics
